0a9bc557d7df93efb91e69f90e98d1008ba8b18f
atorczynski <artur.torcz@gmail.com>
2019-12-17 23:10:47 +0100

    Add topbar component

---
+++ client/src/stories/TopBar.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TopBar from '../components/TopBar';
+
+export default {
+  title: 'TopBar'
+};
+
+export function AppBar() {
+  return <TopBar merchantName={'Merchant'}></TopBar>;
+}
