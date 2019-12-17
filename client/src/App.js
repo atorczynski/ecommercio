6f8cbab8b44dd563c6342638f92a13dcb68916d1
atorczynski <artur.torcz@gmail.com>
2019-12-17 16:56:19 +0100

    Change filename and import new name

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import GlobalStyles from './GlobalStyles';
-import light from './components/themes/theme';
+import light from './components/themes/defaultTheme';
 
 function App() {
   return (
