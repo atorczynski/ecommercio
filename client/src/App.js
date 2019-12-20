7922b710d8847396c2c1c33e76296fb2db7798bf
atorczynski <artur.torcz@gmail.com>
2019-12-20 16:04:19 +0100

    Add pages to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginPage from './pages/Login';
 import { ThemeProvider } from 'emotion-theming';
 import GlobalStyles from './GlobalStyles';
 import light from './themes/defaultTheme';
 import Main from './pages/Main';
+import Import from './pages/Import';
+import TopBar from './components/TopBar';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -15,8 +18,17 @@ function App() {
           <Route exact path="/">
             <LoginPage />
           </Route>
-          <Route exact path="/main">
+          <Route path="/home">
+            <TopBar topText={'Home'} />
             <Main />
+          </Route>
+          <Route path="/import">
+            <TopBar topText={'Import'} />
+            <Import />
+          </Route>
+          <Route path="/products">
+            <TopBar topText={'Products'} />
+            <Products />
           </Route>
         </Switch>
       </Router>
