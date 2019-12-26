f4a0e92ba2b97650e23f2ac7673d900b21032c19
atorczynski <56833518+atorczynski@users.noreply.github.com>
2019-12-26 20:18:48 +0100

    Server/api (#56)
    * Add nodemon and dev script to package.json
    
    * Add simple api router
    
    * Add product schema for future usage with db

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3003;
+
+const apiRoutes = require('./routes/api');
+
+mongoose.connect('mongodb://127.0.0.1:27017/', { useUnifiedTopology: true });
 
 app.listen(port, () => {
   console.log('App running on port: ' + port);
 });
+
+app.use('/api', apiRoutes);
