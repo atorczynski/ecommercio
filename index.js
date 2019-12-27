38ee1e3a5b7e11bc322f71b59285b8501aaf8a80
atorczynski <artur.torcz@gmail.com>
2019-12-27 01:32:19 +0100

    Modify server to allow cors. Extended db connection

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ const app = express();
 const port = 3003;
 
 const apiRoutes = require('./routes/api');
+app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/', { useUnifiedTopology: true });
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+mongoose.connect(
+  'mongodb://127.0.0.1:27017/',
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Conncted to DB');
+    }
+  }
+);
 
 app.listen(port, () => {
   console.log('App running on port: ' + port);
