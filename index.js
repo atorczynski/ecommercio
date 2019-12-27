a4e2f3319a9788186c0754e8736d5cff1b65a987
atorczynski <artur.torcz@gmail.com>
2019-12-27 02:17:55 +0100

    Remove comment from code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const apiRoutes = require('./routes/api');
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
