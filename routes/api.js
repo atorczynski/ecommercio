d902042a45db3a0e8944c0c2134bc04db73e599a
atorczynski <artur.torcz@gmail.com>
2020-01-03 12:09:20 +0100

    add merchant routes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const crawler = require('../controllers/pageCrawler');
+const Products = require('../schemas/productSchema');
 
-router.post('/merchantbase', (req, res) => {
+router.post('/merchantproducts', (req, res) => {
   const baseURL = req.body.params.baseURL;
-  crawler.scrapeURL(baseURL);
+  crawler.scrapeURL(baseURL, []);
+});
+
+router.get('/merchantproducts', async (req, res) => {
+  const products = await Products.find();
+  res.send(products);
+});
+
+router.delete('/merchantproducts:id', async (req, res) => {
+  const product = await Products.findById(req.params.id);
+  res.send(product);
 });
 
 module.exports = router;
