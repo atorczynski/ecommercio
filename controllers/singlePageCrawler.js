bae5903957e8cb3191c2493a3cb5c8138ed45e11
atorczynski <artur.torcz@gmail.com>
2020-01-06 23:20:15 +0100

    Send params to server

---
+++ controllers/singlePageCrawler.js
@@ -0,0 +1,41 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const Product = require('../schemas/productSchema');
+
+function cleanPrice(str) {
+  const removeAfter = str.indexOf('€');
+  const cleanString = str.substring(0, removeAfter);
+  return cleanString;
+}
+
+async function scrapeSingleURL(productURL) {
+  try {
+    console.log(productURL);
+    const res = await axios.get(productURL);
+    const $ = cheerio.load(res.data);
+    if ($('.product--details').length > 0) {
+      const title = $('h1.product--title')
+        .first()
+        .text();
+      const img = $('span img').attr('src');
+      const price = $('.product--price')
+        .first()
+        .text();
+      const numPrice = cleanPrice(price) || -1;
+      const description = $('div.product--description').text();
+
+      console.log(title, description, img, price, numPrice);
+      const crawledProduct = new Product({
+        url: productURL,
+        title: title,
+        description: description,
+        img: img,
+        price: numPrice
+      });
+      await crawledProduct.save();
+    }
+  } catch (error) {
+    console.error('Error: ', productURL, error);
+  }
+}
+exports.scrapeSingleURL = scrapeSingleURL;
