07e5d72b8479e48c715224a9676b6635c1efd9e1
atorczynski <artur.torcz@gmail.com>
2020-01-08 19:04:39 +0100

    Rewrite search function to throttle input on change

--- routes/api.js
+++ routes/api.js
@@ -4,44 +4,6 @@ const crawler = require('../controllers/pageCrawler');
 const pageCrawler = require('../controllers/singlePageCrawler');
 const Products = require('../schemas/productSchema');
 
-router.post('/merchantproducts', (req, res) => {
-  const baseURL = req.body.params.baseURL;
-  crawler.scrapeURL(baseURL, []);
-  res.end();
-});
-
-router.put('/merchantproducts', (req, res) => {
-  try {
-    console.log(req);
-    const url = req.body.params.url;
-    const id = req.body.params.id;
-    console.log(id, url);
-    pageCrawler.scrapeSingleURL(url, id);
-    res.end();
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.get('/merchantproducts', async (req, res) => {
-  const products = await Products.find();
-  res.send(products);
-});
-
-router.get('/search/:query', async (req, res) => {
-  try {
-    const searchQuery = req.params.query;
-    const foundProducts = await Products.find({
-      title: { $regex: '.*' + searchQuery + '.*', $options: 'i' }
-    });
-    console.log(foundProducts);
-    res.send(foundProducts);
-    res.end();
-  } catch (error) {
-    console.error(error);
-  }
-});
-
 router.get('/merchantproducts/:id', async (req, res) => {
   const id = req.params.id;
   const products = await Products.findById(id);
@@ -57,6 +19,38 @@ router.delete('/merchantproducts/:id', async (req, res) => {
   } catch (error) {
     console.error(error);
   }
+});
+
+router.post('/merchantproducts', (req, res) => {
+  const baseURL = req.body.params.baseURL;
+
+  crawler.scrapeURL(baseURL, []);
+  res.end();
+});
+
+router.put('/merchantproducts', async (req, res) => {
+  try {
+    console.log(req);
+    const url = req.body.params.url;
+    const id = req.body.params.id;
+    console.log(id, url);
+    await pageCrawler.scrapeSingleURL(url, id);
+    res.end();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get('/merchantproducts', async (req, res) => {
+  const { q } = req.query;
+
+  const query = q
+    ? {
+        title: { $regex: '.*' + q + '.*', $options: 'i' }
+      }
+    : {};
+  const products = await Products.find(query);
+  res.send(products);
 });
 
 module.exports = router;
