72d02acbcb3f51fd3c330d83c6ab99ceaf8dbe4b
atorczynski <artur.torcz@gmail.com>
2020-01-10 10:12:50 +0100

    Add seperator as component

---
+++ client/src/components/Seperator.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Seperator = styled.hr`
+  width: 220px;
+  height: 0px;
+  margin-top: 15px;
+  border: 1px solid #ed462e;
+  font-family: 'Roboto';
+  margin-top: 35px;
+  opacity: 0.3;
+`;
+
+export default Seperator;
