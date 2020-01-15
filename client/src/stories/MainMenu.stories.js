845fb8b0e26b44521a09f440babcf4885375889b
atorczynski <artur.torcz@gmail.com>
2020-01-15 20:41:42 +0100

    Fix DeepScan issues

--- client/src/stories/MainMenu.stories.js
+++ client/src/stories/MainMenu.stories.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import MenuButton from '../components/general/MenuButton';
 import MainOverview from '../components/MainOverview';
-import { withKnobs, text, number } from '@storybook/addon-knobs';
+import { withKnobs, number } from '@storybook/addon-knobs';
 
 export default {
   title: 'Menu',
