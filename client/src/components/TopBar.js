649096751224d8b77d3d5cc9df84cb87774710e8
atorczynski <artur.torcz@gmail.com>
2020-01-15 13:05:45 +0100

    Remove unused import

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Logo from '../assets/logo.svg';
-import PropTypes from 'prop-types';
 import BurgerButton from './BurgerButton';
 import Menu from './Menu';
 import { Link } from 'react-router-dom';
